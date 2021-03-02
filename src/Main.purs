b7ef428120ef0ba14f4de208b976cf256de56d37
Mike Solomon <mike@meeshkan.com>
2021-03-02 08:11:16 +0200

    Adds sklearn

--- src/Main.purs
+++ src/Main.purs
@@ -6,7 +6,7 @@ import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Class.Console as Log
-import Pytest (appendSysPath, call2, dlOpen, openFile, startInterpreter)
+import Pytest (appendSysPath, call0, dlOpen, openFile, startInterpreter)
 
 main :: Effect Unit
 main =
@@ -16,5 +16,5 @@ main =
       startInterpreter
       appendSysPath "./"
       openFile "test"
-    o <- toAffE (call2 "add" 1 2)
-    Log.info (show (o :: Int))
+    o <- toAffE (call0 "dosklearn")
+    Log.info (o :: String)
--- test.py
+++ test.py
@@ -1,2 +1,28 @@
-def add(a, b):
-  return a + b
+from sklearn import preprocessing, neighbors
+import numpy as np
+
+
+def dosklearn():
+    training_set = {'Dog': [[1, 2], [2, 3], [3, 1]],
+                    'Cat': [[11, 20], [14, 15], [12, 15]]}
+    testing_set = [15, 20]
+
+    x = []
+    y = []
+    for group in training_set:
+        for features in training_set[group]:
+            x.append(features)
+            y.append(group)
+
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(x, y)
+
+    testing_set = np.array(testing_set)
+    testing_set = testing_set.reshape(1, -1)
+
+    prediction = clf.predict(testing_set)
+    return str(prediction[0])
+
+
+if __name__ == '__main__':
+    print(dosklearn())
